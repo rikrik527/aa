ce9692354f31cd33b2e89cac4b020f2b4998194e
rik <rik6827@gmail.com>
2021-01-30 16:49:13 +0800

    kkkk

--- Assets/script/strangers/Strangers.cs
+++ Assets/script/strangers/Strangers.cs
@@ -3,14 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
+using Cinemachine;
 public abstract class Strangers : MonoBehaviour
 {
 
+    [SerializeField]
+    protected Camera camera;
 
+    [SerializeField]
+    protected CinemachineVirtualCamera cam1;
+    [SerializeField]
+    private Transform none;
 
+    private void Init()
+    {
+        cam1 = GameObject.Find("cam1").GetComponent<CinemachineVirtualCamera>();
+    }
+    private void Start()
+    {
+        if (cam1 == null)
+        {
+            Debug.Log("did not cam1");
+            return;
+        }
+    }
 
+    public virtual void Update()
+    {
 
+        // mouse down select target and have canvas menu show up and camera zoom in
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hitInfo = Physics2D.Raycast(worldPoint, Vector2.zero);
+            if (hitInfo.collider != null)
+            {
+                Debug.Log("hitinfo.collider.name" + hitInfo.collider.name);
+                if (hitInfo.collider.tag == "npc")
+                {
+                    Transform target = null;
+                    Debug.Log("clicked npc" + hitInfo.collider.gameObject.transform);
+                    target = hitInfo.collider.gameObject.transform;
+                    cam1.m_Follow = none;
+                    cam1.m_Follow = target;
 
+                }
+            }
+
+        }
+    }
+    public virtual void move()
+    {
+        // let npc have raycast to determine distance
+        // if distance have object npc will walk up and move forward again
+        // if collid with object will have hit each other animation and fall back a little and move up or down
+        // and keep move towards to its destination
+    }
 
 
 
